a196408313a5ed2d445cde2fd885fe029c2ecf66
Abdur Rehman <97359683+abdurrehman587@users.noreply.github.com>
2025-06-17 23:04:30 +0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://suuwywjdccmyaajwfsfd.supabase.co'; // Your Supabase project URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1dXd5d2pkY2NteWFhandmc2ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNTA3NTUsImV4cCI6MjA2NTYyNjc1NX0.UuDC0p6lTBrU5Py6xbxClmHzcivoFCaaZbLdgrdG4fI'; // Your anon key
+const supabaseUrl = 'https://poqarsztryrdlliwjhgx.supabase.co'; // Your Supabase project URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvcWFyc3p0cnlyZGxsaXdqaGd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNTE0NzUsImV4cCI6MjA2NTYyNzQ3NX0.3fkTjLRdfTdIne_uE-m3GoNbu2mxREBlYrraRGX81_4'; // Your anon key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
