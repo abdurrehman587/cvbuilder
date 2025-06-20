8b4e433d770cef11c72e80479a8a501726a1e3c7
Abdur Rehman <97359683+abdurrehman587@users.noreply.github.com>
2025-06-20 19:44:49 +0500

    Update landingpage.js

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 import Template1Preview from './Template1Preview';
 import Template2Preview from './Template2Preview';
